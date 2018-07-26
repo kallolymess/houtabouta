bb5b8085490d6eb84aa0cfd226102b3ae74b2c42
sonnig <kallolymess@yahoo.com>
2018-07-26 18:00:56 +0200

    tasks 001

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,29 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/tasks', function () {
+    $tasks = DB::table('tasks')->get();
+
+    // return $tasks;
+    return view('tasks.tasks', compact('tasks'));
+});
+
+Route::get('/tasks/{task}', function ($what) {
+
+    // dd($what);
+
+    $task = DB::table('tasks')->find($what);
+
+    // dd($tasks);
+
+    // return $tasks;
+    return view('tasks.show', compact('task'));
 });
 
 // this produces the phpinfo() for http://localhost:8888/info from MAMP OR http://localhost:8000/info for artisan serve
 Route::get('/info', function () {
     return view('phpInfo');
+});
+Route::get('/', function () {
+    return view('welcome');
 });
