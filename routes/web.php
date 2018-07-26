9b96700bd99138621ee032de86c563af087d0b10
sonnig <kallolymess@yahoo.com>
2018-07-27 00:22:52 +0200

    created the Post model controller and migration

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'PostsController@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// this produces the phpinfo() for http://localhost:8888/info from MAMP OR http://localhost:8000/info for artisan serve
-Route::get('/info', function () {
-    return view('phpInfo');
-});
+// Eloquent Model => Post
+// migration => create_posts_table
+// controller => PostsController
